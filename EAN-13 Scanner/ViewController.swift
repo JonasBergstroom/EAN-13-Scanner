ea181499f7b03f7aaf3d4d885798878f13c4bf70
jonasbergstrom <jontrian97@gmail.com>
2022-09-01 22:17:06 +0200

    Added the StartScanScreen function and the metadataOutput function to be able to use the camera for EAN13 barcodes recognition.

--- EAN-13 Scanner/ViewController.swift
+++ EAN-13 Scanner/ViewController.swift
@@ -14,12 +14,18 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     @IBOutlet weak var lightButton: UIButton!
     @IBOutlet weak var newScanButton: UIButton!
     
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
     private var torch = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        StartScanScreen()
+        view.isUserInteractionEnabled = true
     
     }
+    
     func toggleTorch(on: Bool) {
         guard let device = AVCaptureDevice.default(for: .video) else { return }
         if device.hasTorch {
@@ -31,6 +37,7 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
                 } else {
                     device.torchMode = .off
                 }
+
                 device.unlockForConfiguration()
             } catch {
                 print("Torch could not be used")
@@ -44,6 +51,53 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
         torch.toggle()
         toggleTorch(on: torch)
     }
+    
+    func StartScanScreen(){
+        
+        let metadataOutput = AVCaptureMetadataOutput()
 
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .high
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            return
+        }
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            //Format of the barcode.
+            metadataOutput.metadataObjectTypes = [.ean13]
+        } else {
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = self.view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+
+        captureSession.startRunning()
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+        }
+
+        dismiss(animated: true)
+    }
 }
 
