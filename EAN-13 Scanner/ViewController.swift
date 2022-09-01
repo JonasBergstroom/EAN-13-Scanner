ca6b5124985d526ed9eca31ca7d04413c9f29ab9
jonasbergstrom <jontrian97@gmail.com>
2022-09-01 19:15:10 +0200

    Initial Commit

---
+++ EAN-13 Scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EAN-13 Scanner
+//
+//  Created by Jonas Bergström on 2022-09-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
