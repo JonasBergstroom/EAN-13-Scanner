c8ced5a48cc743e6b0098c5a19c4f5c86edc14d6
jonasbergstrom <jontrian97@gmail.com>
2022-09-01 21:57:52 +0200

    Imported the AVFoundation and added the toggleTorch function

--- EAN-13 Scanner/ViewController.swift
+++ EAN-13 Scanner/ViewController.swift
@@ -6,18 +6,44 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
     @IBOutlet weak var scanScreenView: UIImageView!
     @IBOutlet weak var lightButton: UIButton!
     @IBOutlet weak var newScanButton: UIButton!
     
+    private var torch = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
 
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+    
+    @IBAction func flashLight(_ sender: UIButton) {
+        torch.toggle()
+        toggleTorch(on: torch)
+    }
 
 }
 
